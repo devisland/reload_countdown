0acfb3dd58813883ef00e63056fbea79cb0f2e05
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2012-04-21 11:55:09 -0300

    Renato Edson

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -1,5 +1,6 @@
 class Gun
   attr_reader :wait_time
+  attr_reader :reload_time
 
   def initialize(wait_time)
     @wait_time = wait_time
@@ -7,11 +8,16 @@ class Gun
 
   def state
     if @last_shot_at.nil? or
-       (Time.now - @last_shot_at >= @wait_time)
+       (Time.now - @last_shot_at >= @wait_time) or
+          (@reload_time == 0)
       return :ready
     end
 
     :reloading
+  end
+
+  def timeElapsed(seconds)
+    @reload_time = @reload_time - seconds;
   end
 
   def shot()
