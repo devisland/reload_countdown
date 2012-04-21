872805fb0d330ffed6f65fcc952340e586d472da
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2012-04-21 12:56:10 -0300

    JV changed rspec idiom

--- spec/lib/dojo_spec.rb
+++ spec/lib/dojo_spec.rb
@@ -2,43 +2,46 @@ require "spec_helper"
 
 describe Gun do
   context "a gun with wait_time of 2 seconds" do
-    subject{Gun.new(2)}
+    let(:gun){Gun.new(2)}
+    subject{gun}
+
     its(:wait_time){should be(2)}
-    it "starts ready" do
-      subject.should be_ready
+
+    it do
+      should be_ready
     end
 
-    it "shot" do
-      subject.shot.should be_true
+    it "can shot" do
+      gun.shot.should be_true
     end
 
-    it "after shot" do
-      subject.shot
-      subject.should be_reloading
-    end
+    context "after shot" do
+      before do
+        gun.shot
+      end
 
-    it "cannot shot two times in a row " do
-      subject.shot
-      subject.shot.should be_false
-    end
+      it do
+        gun.should be_reloading
+      end
 
-    it "shot after 2 seconds" do
-      subject.shot
-      subject.time_elapsed(2)
-      subject.shot.should be_true
-    end
+      it "cannot shot two times in a row " do
+        gun.shot.should be_false
+      end
 
-    it "shot after 10 seconds" do
-      subject.shot
-      subject.time_elapsed(10)
-      subject.shot.should be_true
-    end
+      it "shot after 2 seconds" do
+        gun.time_elapsed(2)
+        gun.shot.should be_true
+      end
 
-    it "can not shot after 1 second" do
-      subject.shot
-      subject.time_elapsed(1)
-      subject.shot.should be_false
-    end
+      it "shot after 10 seconds" do
+        gun.time_elapsed(10)
+        gun.shot.should be_true
+      end
 
+      it "can not shot after 1 second" do
+        gun.time_elapsed(1)
+        gun.shot.should be_false
+      end
+    end
   end
 end
