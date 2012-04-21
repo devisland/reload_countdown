9efda16e34938276a4e1a80a69d527b8c331824d
João Vitor Lacerda Guimarães <joao.vitor@gmail.com>
2012-04-21 10:50:49 -0300

    JV Gibran

--- lib/dojo.rb
+++ lib/dojo.rb
@@ -1,2 +1,6 @@
-class Dojo
+class Gun
+  attr_reader :wait_time
+  def initialize(wait_time)
+    @wait_time = wait_time
+  end
 end
--- spec/lib/dojo_spec.rb
+++ spec/lib/dojo_spec.rb
@@ -1,7 +1,11 @@
 require "spec_helper"
 
-describe Dojo do
-  it do
-    pending
+describe Gun do
+  context "a gun with wait_time of 2 seconds" do
+    subject{Gun.new(2)}
+    its(:wait_time){should be(2)}
+    it "starts stopped" do
+      subject.stopped.should be_true
+    end
   end
 end
